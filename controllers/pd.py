5b23fb7b681d0a6982561f31a26cf455da465d36
danak212 <danak112@wp.pl>
2024-12-09 23:35:07 +0100

    TASK 3 working!!!

--- controllers/pd.py
+++ controllers/pd.py
@@ -3,22 +3,23 @@ from controllers.interface import Controller
 
 
 class PDController(Controller):
-    def __init__(self, env, kp, kd):
+    def __init__(self, env, kp=10.0, kd=1.0):
         super().__init__(env)
         self.kp = kp  # Wzmocnienie proporcjonalne
-        self.kd = kd  # Wzmocnienie różniczkowe
+        self.kd = kd  # Wzmocnienie różniczkujące
 
     def compute_control(self, state):
-        cos_theta, sin_theta, theta_dot = state  # Trzy elementy observation
-        theta = np.arctan2(sin_theta, cos_theta)  # Przeliczamy kąt
-
-        # Implementacja regulatora PD
-        desired_theta = 0  # Chcemy, by wahadło stało pionowo
-        error = desired_theta - theta  # Błąd pozycji
-        d_error = -theta_dot  # Prędkość kątowa to "pochodna" kąta
-
-        # Sterowanie zgodne z formułą PD
-        action = self.kp * error + self.kd * d_error
-
-        # Gym wymaga, by action było w formacie listy lub tablicy NumPy
-        return [action]  # Zwracamy action jako listę
+        """
+        Oblicza sterowanie w oparciu o aktualny stan.
+        Stan jest listą [cos(theta), sin(theta), theta_dot], gdzie:
+        - cos(theta), sin(theta) to kosinus i sinus kąta
+        - theta_dot to prędkość kątowa
+        """
+        cos_theta, sin_theta, theta_dot = state  # Rozpakowanie stanu
+        theta = np.arctan2(sin_theta, cos_theta)  # Odzyskujemy kąt theta
+        # Sterowanie oparte na równaniu PD
+        control = -self.kp * theta - self.kd * theta_dot
+        # Ograniczenie momentu do zakresu sterowania środowiska
+        max_torque = self.env.action_space.high[0]  # Maksymalny moment
+        control = np.clip(control, -max_torque, max_torque)
+        return [control]  # Zwracamy jako lista (wymaga tego step())
